8c54ce15f71cab208a0b318e09fd964a4683e443
DeveloperRucel07 <tsafackrucel@gmail.com>
2026-07-26 20:44:23 +0200

    added db backups and edited nginx domain and port

---
+++ scripts/backups.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+BACKUP_DIR="/opt/einfachpatho/backups"
+mkdir -p "$BACKUP_DIR"
+
+FILENAME="$BACKUP_DIR/einfachpatho_$(date +%Y%m%d_%H%M%S).sql.gz"
+docker exec einfachpatho_postgres pg_dumpall -U einfachpatho | gzip > "$FILENAME"
+find "$BACKUP_DIR" -type f -mtime +30 -delete
+
+echo "[SUCCESS] Backup gespeichert unter: $FILENAME"
